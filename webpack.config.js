c7f0414ae51ddea1393fabad01fa2fa34bd83ad2
SerSerch <serserch@yandex.ru>
2019-04-02 14:50:03 +0300

    Add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ const path = require('path'),
     HTMLplugin = require('html-webpack-plugin'),
     MiniCssExtractPlugin = require('mini-css-extract-plugin'),
     UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
-    OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'),
-    SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+    OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     entry:{
@@ -37,16 +36,6 @@ module.exports = {
                 ],
             },
             {
-                test: /(mobile|critical).styl$/,
-                use: [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'stylus-loader'
-                ]
-            },
-            {
                 test: /\.(gif|png|jpe?g)$/,
                 use: [
                     {
@@ -58,35 +47,12 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(svg)$/,
-                use: [
-                    {
-                        loader: 'svg-sprite-loader',
-                        options: {
-                            extract: true,
-                            spriteFilename: 'img/sprite-[hash:6].svg'
-                        }
-                    },
-                    {
-                        loader: 'svgo-loader',
-                        options: {
-                            plugins: [
-                                { removeTitle: true },
-                                { convertColors: { shorthex: false } },
-                                { convertPathData: false }
-                            ]
-                        }
-                    },
-                    'svg-transform-loader',
-                ]
-            },
-            {
-                test: /\.(eot|otf|ttf|woff|woff2)$/,
+                test: /\.svg$/,
                 use: [
                     {
                         loader: 'file-loader',
                         options: {
-                            name: 'fonts/[hash].[ext]'
+                            name: 'svg/[name].[ext]'
                         }
                     }
                 ]
@@ -110,11 +76,9 @@ module.exports = {
     plugins :[
         new HTMLplugin({
             template: path.resolve(__dirname, 'src', 'pages', 'index.pug'),
-            filename: 'index.html',
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css',
-        }),
-        new SpriteLoaderPlugin(),
+        })
     ],
 };
